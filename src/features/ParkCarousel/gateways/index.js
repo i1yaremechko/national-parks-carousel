ced59f1a5893a4774fcd9c3dd04ad778dcb5d624
vnSasa <ol.yaremechko@gmail.com>
2026-06-25 11:25:10 +0300

    move API key from query params to request headers

--- src/features/ParkCarousel/gateways/index.js
+++ src/features/ParkCarousel/gateways/index.js
@@ -1,8 +1,12 @@
 import { API_KEY, BASE_URL } from '@common/constants';
 
 export const fetchParksFromServer = async () => {
-  const url = `${BASE_URL}/parks?limit=10&api_key=${API_KEY}`;
-  const response = await fetch(url);
+  const url = `${BASE_URL}/parks?limit=10`;
+  const response = await fetch(url, {
+    headers: {
+      'X-Api-Key': API_KEY
+    },
+  });
 
   if (!response.ok) {
     throw new Error(`NPS API error: Server returned status ${response.status}`);
